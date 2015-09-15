c2c1a3abde6accdf0c8c27cf06c929c83d5db6c7
Mirim <mirim@e-mirim.hs.kr>
2015-09-15 17:26:00 +0900

    TCP

---
+++ TCP/src/TCPClientMain.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * Created by Mirim on 2015-09-15.
+ */
+public class TCPClientMain {
+    public static void main(String[] args) throws IOException {
+        //input message
+        String message = "";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("보낼 메세지 > ");
+        message=sc.nextLine();
+
+        //make socket
+        Socket socket = new Socket("localhost", 1019);
+
+        //make writer
+        PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+
+        //write
+        writer.println(message);
+    }
+}
